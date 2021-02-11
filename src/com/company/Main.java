7c13561b4644e679abb9c2c01e3359bb8ea9a1c5
Atri Hegde <iamatrihegde@gmail.com>
2021-02-11 18:44:23 +0000

    scenes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
+/*
+btn.setOnAction(e -> System.out.println("Hey now"));  this is a lambda expression. Java already know through the context of
+                                                        of the code to treat it as an event handler
+
+ */
--- src/com/company/forGUI.java
+++ src/com/company/forGUI.java
@@ -6,11 +6,15 @@ import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class forGUI extends Application implements EventHandler<ActionEvent> {
-    Button btn;
+public class forGUI extends Application{
+
+    Stage window;
+    Scene scene1, scene2;
 
     public static void main(String[] args) {
         launch(args);
@@ -18,23 +22,31 @@ public class forGUI extends Application implements EventHandler<ActionEvent> {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("Hello World!");
-        btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(this);
+        window = primaryStage;
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(btn);
+        Label label1 = new Label("welcome to the first scene!");
+        Button button1 = new Button("Go to scene 2");
+        button1.setOnAction(e -> window.setScene(scene2));
 
-        primaryStage.setScene(new Scene(layout, 300, 250));
-        primaryStage.show();
+        //Layout 1 = children are laid out in vertical column
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1, button1);
+        scene1 = new Scene(layout1, 200, 200);
+
+        Button button2 = new Button("This scene sucks, go back to scene 1");
+        button2.setOnAction(e -> window.setScene(scene1));
+
+        //Layout 2
+        StackPane layout2 = new StackPane();
+        layout2.getChildren().add(button2);
+        scene2 = new Scene(layout2, 600, 300);
+
+        window.setScene(scene1);
+        window.setTitle("Title here");
+        window.show();
+
+
+
     }
 
-    @Override
-    public void handle(ActionEvent event) {
-        if(event.getSource()==btn){
-            System.out.println("Hello, it works");
-        }
-
-    }
 }
