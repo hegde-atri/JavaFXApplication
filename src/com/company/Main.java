5a34f8739aa4d40f7267132469bdeabdc4f4dc3e
Atri Hegde <iamatrihegde@gmail.com>
2021-02-12 18:08:15 +0000

    embedded layouts

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,19 +3,46 @@ package com.company;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    Stage window;
-    Button button;
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
+    public void start(Stage window) {
+        window.setTitle("Title");
+
+        HBox topMenu = new HBox();
+        Button buttonA = new Button("File");
+        Button buttonB = new Button("Edit");
+        Button buttonC = new Button("View");
+        topMenu.getChildren().addAll(buttonA, buttonB, buttonC);
+
+        VBox leftMenu = new VBox();
+        Button buttonD = new Button("D");
+        Button buttonE = new Button("E");
+        Button buttonF = new Button("F");
+        leftMenu.getChildren().addAll(buttonD, buttonE, buttonF);
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setTop(topMenu);
+        borderPane.setLeft(leftMenu);
+
+        Scene scene = new Scene(borderPane, 300, 250);
+        window.setScene(scene);
+        window.show();
+    }
+
+}
+/*
     public void start(Stage primaryStage) {
         window = primaryStage;
         window.setTitle("Hello");
@@ -41,6 +68,6 @@ public class Main extends Application {
         if(answer){
             window.close();
         }
-    }
-}
+
+ */
 
